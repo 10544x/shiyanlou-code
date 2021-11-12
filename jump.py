55ed2882f6da744515c083e2ec77ff9c6bf64333
ygy <1392381333@qq.com>
2021-11-12 19:37:57 +0800

    修改

--- jump.py
+++ jump.py
@@ -1,3 +1,9 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   jump.py
+@Create Time : 2021/11/12 17:36
+@Author :ygy
+'''
 for i in range(1,101):
     if i%7!=0 and i%10!=7 and i//10!=7:
         print(i)
